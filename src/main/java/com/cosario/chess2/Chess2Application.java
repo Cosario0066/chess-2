d0b586ec6625c17c86ed0b5b1b5949a28b076b56
Cosario <lucas_leturque@outlook.fr>
2026-09-18 11:48:47 +0200

    init

---
+++ src/main/java/com/cosario/chess2/Chess2Application.java
@@ -0,0 +1,13 @@
+package com.cosario.chess2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chess2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chess2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cosario/chess2/Chess2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosario.chess2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chess2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
